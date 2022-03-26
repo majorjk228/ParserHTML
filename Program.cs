0b710e41b011e5eadc992824ff224a25e64be8b6
majorjk228 <sana-ivanov1@yandex.ru>
2022-03-26 18:51:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParserHTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
